1c3cf39949997696904091c6a97d5fc0257701b8
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-04-12 00:32:10 +0300

    changed build.gradle for junit4 + added tests

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+}
