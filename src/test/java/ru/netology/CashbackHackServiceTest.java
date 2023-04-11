6e2de342a3d66fbdfc7b9044e7d1d405b586d4d2
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-04-12 00:19:02 +0300

    changed build.gradle + added tests

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+}
